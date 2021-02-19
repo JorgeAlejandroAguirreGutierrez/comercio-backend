3eee2ecfd782a6e4c8411bdafae5be25bea57dd0
alejandro <JAGUIR4@bancodebogota.com.co>
2021-02-19 09:34:26 -0500

    mod

--- src/test/java/com/backend/comercio/ComercioApplicationTests.java
+++ src/test/java/com/backend/comercio/ComercioApplicationTests.java
@@ -1,10 +1,10 @@
-package com.backend.ciudadvictoria;
+package com.backend.comercio;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CiudadvictoriaApplicationTests {
+class ComercioApplicationTests {
 
 	@Test
 	void contextLoads() {
