c8c7cdb7b424ea2dd7a5b9dc6d840ad65423a051
alejandro <JAGUIR4@bancodebogota.com.co>
2021-03-05 21:08:07 -0500

    mod

--- src/main/java/com/backend/comercio/WebConfigurer.java
+++ src/main/java/com/backend/comercio/WebConfigurer.java
@@ -10,6 +10,8 @@ import org.springframework.web.servlet.config.annotation.*;
 public class WebConfigurer implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    	registry.addResourceHandler("/storage/**").addResourceLocations("file://" + System.getProperty("user.dir") + "/src/main/upload/");
+    	registry.addResourceHandler("/img/**").addResourceLocations("file://" + System.getProperty("user.dir") + "/src/main/img/");
+    	registry.addResourceHandler("/imgqr/**").addResourceLocations("file://" + System.getProperty("user.dir") + "/src/main/imgqr/");
+    	registry.addResourceHandler("/imgfront/**").addResourceLocations("file://" + System.getProperty("user.dir") + "/src/main/imgfront/");
     }
 }
